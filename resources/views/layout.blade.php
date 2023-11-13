2cfe4559dedfde9d1acd079d28f4a514c9da1bf5
Tanvir <34455693+tanvirlaravel@users.noreply.github.com>
2023-11-13 15:54:21 +0600

    3.5 done

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -8,8 +8,8 @@
     <body>
 
         <ul>
-            <li><a href="/">Home</a></li>
-            <li><a href="/about">About</a></li>
+            <li><a href="{{ route('home') }}">Home</a></li>
+            <li><a href="{{ route('about') }}">About</a></li>
         </ul>
 
         <div  class="main">
