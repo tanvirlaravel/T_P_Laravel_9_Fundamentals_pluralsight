394ffb8d5a8c5664fd4452b490a7aaae75d3120c
Tanvir <34455693+tanvirlaravel@users.noreply.github.com>
2023-11-15 17:36:45 +0600

    6.5 & 6.6 done

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function home() {
-        return view('home');
+        $posts = Post::all();
+        return view('home', ['posts' => $posts]);
     }
 
     public function about() {
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,6 +3,19 @@
 @section('title', 'Home')
 
 @section('content')
-<h1>Home page</h1>
-<p>This is the home page.</p>
+
+@forelse ($posts as $post )
+
+<div class="post-item">
+    <div class="post-content">
+        <h2>{{ $post->title }}</h2>
+        <p>{{ $post->description }}</p>
+    </div>
+</div>
+
+@empty
+<h2>There are no post yet.</h2>
+@endforelse
+
+
 @endsection
