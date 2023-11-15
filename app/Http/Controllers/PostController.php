c367b89f987c620004280006e7a64578c9b69d98
Tanvir <34455693+tanvirlaravel@users.noreply.github.com>
2023-11-15 17:25:31 +0600

    6.4 done

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
@@ -29,9 +30,17 @@ class PostController extends Controller
             'title' => 'required',
             'description' => ['required', 'min:10']
         ]);
+
+        $post = new Post();
+
+        $post->title = $request->input('title');
+        $post->description = $request->input('description');
+
+        $post->save();
+
         return redirect()
                 ->route('posts.create')
-                ->with('success', 'Post is submitted! Title: ' . $request->input('title') . 'Descriptiosn: ' .$request->input('description'));
+                ->with('success', 'Post is submitted! Title: ' . $post->title . 'Descriptiosn: ' .$post->description);
     }
 
     /**
