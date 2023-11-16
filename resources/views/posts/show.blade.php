8cfeb79f360a1a40e7412bab969a4e2130947ce5
Tanvir <34455693+tanvirlaravel@users.noreply.github.com>
2023-11-16 10:14:49 +0600

    7.6  done

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -9,6 +9,11 @@
         <h2>{{ $post->title }}</h2>
         <p>{{ $post->description }}</p>
         <a href="{{ route('posts.edit', [$post]) }}" >Edit </a>
+        <form  method="POST" action="{{ route('posts.destroy', [$post]) }}" >
+            @csrf
+            @method('DELETE')
+            <button class="delete" type="submit">Delete</button>
+        </form>
     </div>
 </div>
 @endsection
