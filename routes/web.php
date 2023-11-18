1742a5c0808da07d75f2235c6b413ff52806504c
Tanvir <34455693+tanvirlaravel@users.noreply.github.com>
2023-11-18 09:31:50 +0600

    9.7 done

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,9 @@ Route::get('/', [HomeController::class, 'home'] )->name('home');
 
 Route::get('/about', [HomeController::class, 'about'])->name('about');
 
-Route::resource('posts', PostController::class)->except(['index']);
+Route::resource('posts', PostController::class)->except(['index'])->middleware('auth');
 
 
-Route::match(['get', 'post'], '/register', [AuthController::class, 'register'])->name('register');
-Route::match(['get', 'post'], '/login', [AuthController::class, 'login'])->name('login');
+Route::match(['get', 'post'], '/register', [AuthController::class, 'register'])->name('register')->middleware('guest');
+Route::match(['get', 'post'], '/login', [AuthController::class, 'login'])->name('login')->middleware('guest');
 Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
