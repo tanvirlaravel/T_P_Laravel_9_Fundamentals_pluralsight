0794516087df53f3baa50c65932fdee2f82112c8
Tanvir <34455693+tanvirlaravel@users.noreply.github.com>
2023-11-18 08:40:27 +0600

    9.3 done

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
@@ -12,21 +13,5 @@ Route::get('/about', [HomeController::class, 'about'])->name('about');
 
 Route::resource('posts', PostController::class)->except(['index']);
 
-// Route::name('posts.')->prefix('posts')->group(function(){
 
-//     Route::get('/create', function(){
-//         return view('posts.create');
-//     })->name('create');
-
-//     Route::post('/', function(Request $request){
-
-//         $request->validate([
-//             'title' => 'required',
-//             'description' => ['required', 'min:10']
-//         ]);
-//         return redirect()
-//                 ->route('posts.create')
-//                 ->with('success', 'Post is submitted! Title: ' . $request->input('title') . 'Descriptiosn: ' .$request->input('description'));
-
-//     })->name('store');
-// });
+Route::match(['get', 'post'], '/register', [AuthController::class, 'register'])->name('register');
