5e7a76683fbabf46891f75c8b0428785032f301d
Tanvir <34455693+tanvirlaravel@users.noreply.github.com>
2023-11-18 08:47:53 +0600

    9.4 done

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -11,6 +13,22 @@ class AuthController extends Controller
         if ($request->isMethod('get')) {
             return view('auth.register');
         }
+
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required:min:1',
+        ]);
+
+        User::create([
+            'name'  => $request->input('name'),
+            'email' => $request->input('email'),
+            'password' => Hash::make($request->input('password')),
+        ]);
+
+        return redirect()
+            ->route('login')
+            ->with('success', 'Your account has been created! You can log in now');
 
     }
 
